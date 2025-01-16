f501eb84f251e55ab92baa4f63aa88179ef27f3b
rbltone <anthonyperez26@hotmail.com>
2025-01-15 19:58:59 -0800

    Add project files.

---
+++ COMPA.LectureActivity1/Program.cs
@@ -0,0 +1,14 @@
+﻿//namespace level
+namespace COMPA.LectureActivity1
+{
+    //class level
+    internal class Program
+    {
+        //main level
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
