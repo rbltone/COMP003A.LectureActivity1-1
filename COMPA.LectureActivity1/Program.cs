59fa4c9dc4c95471f0e20c7d0af2c79614d50a6e
rbltone <anthonyperez26@hotmail.com>
2025-01-15 20:52:28 -0800

    end of class prog.

--- COMPA.LectureActivity1/Program.cs
+++ COMPA.LectureActivity1/Program.cs
@@ -4,6 +4,7 @@
     Facuty: Jonathan Cruz
     Purpose: basic console app
  */
+
 //namespace level
 namespace COMPA.LectureActivity1
 {
@@ -13,8 +14,16 @@ namespace COMPA.LectureActivity1
         //main level
         static void Main(string[] args)
         {
+            //Prompt User Input
+            Console.Write("Enter your name: ");
+            string username = Console.ReadLine();   //gets the user input and saves it to a variable
+
+            Console.WriteLine("Hi " + username + "My name is Anthony");
             
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1. I am an engineering student  \n 2.\n 3. \n 4. \n 5. \n 6. \n 7. \n 8. \n 9.\n 10. ");
+
+            
+
         }
     }
 }
